bcccf796618eddba74bd5ecea2b5a09de207c993
pritom48 <pritamdas41148@gmail.com>
2022-04-04 12:47:35 +0600

    reviews added

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -13,6 +13,68 @@ const Home = () => {
                     <img className=' img-fluid' src="https://media.istockphoto.com/photos/illustration-of-generic-hatchback-car-perspective-view-picture-id1150425295?b=1&k=20&m=1150425295&s=170667a&w=0&h=6RQC_JidCDrQYd_9JHFC5ANGp70KBg9nj_TpEDu-NZk=" alt="" />
                 </div>
             </div>
+            <div>
+                <h1 className='text-center'>Customer Reviews</h1>
+                <div className="row row-cols-1 row-cols-md-3 g-4">
+                    <div className="col">
+                        <div className="card h-100">
+
+                            <div className="card-body">
+                                <h5 className="card-title text-center">Card title</h5>
+                                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer. This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card h-100">
+
+                            <div className="card-body">
+                                <h5 className="card-title text-center">Card title</h5>
+                                <p className="card-text">This is a short card.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card h-100">
+
+                            <div className="card-body">
+                                <h5 className="card-title text-center">Card title</h5>
+                                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card h-100">
+
+                            <div className="card-body">
+                                <h5 className="card-title text-center">Card title</h5>
+                                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card h-100">
+
+                            <div className="card-body">
+                                <h5 className="card-title text-center">Card title</h5>
+                                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card h-100">
+
+                            <div className="card-body">
+                                <h5 className="card-title text-center">Card title</h5>
+                                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <button type="button" className="btn btn-primary my-5 d-flex align-items-center">See All Reviews</button>
+            </div>
         </div>
     );
 };
